b457f801b298019041f4bf7b713bf1d768fe3056
kim <kmichel.jae@gmail.com>
2023-01-10 18:06:32 -0300

    day 8 - function parameters & caesar cipher

---
+++ 008/main.py
@@ -0,0 +1,36 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(text, shift, action):
+    end_text = ""
+    if action == "decode":
+        shift *= -1
+        
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {action}d result: {end_text}")
+
+print(logo)
+
+should_end = False
+while not should_end:
+    action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(text= text, shift= shift, action= action)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
+
+    
